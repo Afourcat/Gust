349fd75ade23075ef9829cad1db72abe7541c17e
alex <alexandre.fourcat@epitech.eu>
2018-08-16 22:21:32 +0200

    Color unit test

--- src/color.rs
+++ src/color.rs
@@ -90,3 +90,41 @@ impl Default for Color {
         Color (1.0, 1.0, 1.0, 1.0)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use color::Color;
+
+    #[test]
+    fn color_new() {
+        let blue = Color::new(0.0, 0.0, 1.0);
+        assert!(blue.2 == 1.0);
+        assert!(blue.0 == 0.0);
+    }
+
+    #[test]
+    fn color_add() {
+        let red = Color::new(1.0, 0.0, 0.0);
+        let blue = Color::new(0.0, 0.0, 1.0);
+        let purple = red + blue;
+
+        assert!(purple.0 == 1.0 && purple.2 == 1.0);
+    }
+
+    #[test]
+    fn color_sub() {
+        let green = Color::new(0.0, 1.0, 0.0);
+        let marron = Color::new(0.2, 0.2, 0.2);
+        let sub = green - marron;
+
+        assert!(sub.0 == -0.2 && sub.1 == 0.8);
+    }
+
+    fn color_multiply() {
+        let black = Color::new(0.0, 0.0, 0.0);
+        let rdm = Color::new(0.4, 0.0, 0.6);
+        let multiply = black * rdm;
+
+        assert!(multiply.0 == 0.0 && multiply.2 == 0.0);
+    }
+}
